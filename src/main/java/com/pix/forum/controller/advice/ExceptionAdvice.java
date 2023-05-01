b0aef1f7fdd1b23eed0d3e6c290cea241fa97186
pix <1196499401@qq.com>
2023-05-01 14:44:49 +0800

    统一处理异常

---
+++ src/main/java/com/pix/forum/controller/advice/ExceptionAdvice.java
@@ -0,0 +1,37 @@
+package com.pix.forum.controller.advice;
+
+import com.pix.forum.util.CommunityUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @Author pix
+ * @Date 2023/5/1 14:35
+ */
+@Slf4j
+@ControllerAdvice(annotations= Controller.class)
+public class ExceptionAdvice {
+    @ExceptionHandler({Exception.class})
+    public void handlerException(Exception e, HttpServletResponse response, HttpServletRequest request) throws IOException {
+        log.error("服务器发生异常" + e.getMessage());
+        for (StackTraceElement element : e.getStackTrace()) {
+            log.error(element.toString());
+        }
+
+        String xRequestedWith = request.getHeader("x-requested-with");
+        if ("XMLHttpRequest".equals(xRequestedWith)) { // 异步请求
+            response.setContentType("application/plain;charset=utf-8");
+            PrintWriter writer = response.getWriter();
+            writer.write(CommunityUtil.getJsonString(1, "服务器异常！"));
+        } else {
+            response.sendRedirect(request.getContextPath() + "/error");
+        }
+    }
+}
